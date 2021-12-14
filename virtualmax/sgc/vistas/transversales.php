1f97b32a932bbfccb4d48ed16580b73920b60278
git <oc.sistemas@mediqboy.com>
2021-12-14 16:18:57 -0500

    cuatro

---
+++ virtualmax/sgc/vistas/transversales.php
@@ -0,0 +1,164 @@
+  <div class="container">    
+    <table class="table table-bordered table-condensed table-hover table-responsive table-striped">
+      <caption><h3>Documentos Transversales</h3></caption>
+      <thead>
+        <tr class="bg-blue">
+          <th class="text-center">#</th>
+          <th class="text-center">Código</th>
+          <th class="text-center">Nombre</th>
+          <th class="text-center"> Documento</th>
+        </tr>
+      </thead>      
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>MQ-MC-PLT-001</td>
+          <td class="text-left">Política de Calidad</td>
+          <td>
+            <a href="archivos/MQ-MC-PLT-001.pdf?v2" target="_blank"> 
+              <button type="button" class="btn btn-xs btn-success">Ver</button>
+            </a>
+          </td>
+        </tr> 
+ 
+        <tr>
+          <td>2</td>
+          <td>MQ-MC-MAN-001</td>
+          <td class="text-left">Manual de Calidad ISO 9001 Company Mediqboy</td>
+          <td>
+            <a href="archivos/MQ-MC-MAN-001.pdf" target="_blank"> 
+              <button type="button" class="btn btn-xs btn-success">Ver</button>
+            </a>
+          </td>
+        </tr> 
+        
+        <tr>
+          <td>3</td>
+          <td>MQ-MC-PRO-001</td>
+          <td class="text-left">Normal Fundamental</td>
+          <td>
+            <a href="archivos/MQ-MC-PRO-001.pdf" target="_blank"> 
+              <button type="button" class="btn btn-xs btn-success">Ver</button>
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>MQ-MC-FOR-001</td>
+          <td class="text-left">Registro de Asistencia a Eventos Organizacionales</td>
+          <td>
+            <a href="archivos/MQ-MC-FOR-001-1.xlsx" target="_blank"> 
+              <button type="button" class="btn btn-xs btn-success">Ver</button>
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>MQ-MC-FOR-002</td>
+          <td class="text-left">Matriz de Identificación de Stakeholders</td>
+          <td>
+            <a href="archivos/MQ-MC-FOR-002.xlsx" target="_blank"> 
+              <button type="button" class="btn btn-xs btn-success">Ver</button>
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td>6</td>
+          <td>MQ-MC-FOR-004</td>
+          <td class="text-left">Matriz de Requisitos Legales</td>
+          <td>
+            <a href="archivos/MQ-MC-FOR-004.xlsx" target="_blank"> 
+              <button type="button" class="btn btn-xs btn-success">Ver</button>
+            </a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>7</td>
+          <td>MQ-MC-FOR-006</td>
+          <td class="text-left">Matriz de Identificación de Stakeholders</td>
+          <td>
+            <a href="archivos/MQ-MC-FOR-006.pdf" target="_blank"> 
+              <button type="button" class="btn btn-xs btn-success">Ver</button>
+            </a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>8</td>
+          <td>MQ-MC-FOR-007</td>
+          <td class="text-left">Matriz de Requisitos Legales</td>
+          <td>
+            <a href="archivos/MQ-MC-FOR-007.pdf?v2" target="_blank"> 
+              <button type="button" class="btn btn-xs btn-success">Ver</button>
+            </a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>9  </td>
+          <td>MQ-MC-FOR-009</td>
+          <td class="text-left">Listado Maestro de Documento</td>
+          <td>
+            <a href="archivos/MQ-MC-FOR-009.xlsx" target="_blank"> 
+              <button type="button" class="btn btn-xs btn-success">Ver</button>
+            </a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>10</td>
+          <td>MQ-MC-FOR-012</td>
+          <td class="text-left">Matriz de Riesgos y Oportunidades</td>
+          <td>
+            <a href="archivos/MQ-MC-FOR-012.pdf" target="_blank"> 
+              <button type="button" class="btn btn-xs btn-success">Ver</button>
+            </a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>11</td>
+          <td></td>
+          <td class="text-left">Membrete Oficial Company Mediqboy OC SAS</td>
+          <td>
+            <a href="archivos/MEMBRETE OFICIAL COMPANY MEDIQBOY.docx" target="_blank"> 
+              <button type="button" class="btn btn-xs btn-success">Ver</button>
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td>12</td>
+          <td>MQ-DE-DIR-001</td>
+          <td class="text-left">Directiva</td>
+          <td>
+            <a href="archivos/MQ-DE-DIR-001.docx" target="_blank"> 
+              <button type="button" class="btn btn-xs btn-success">Ver</button>
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td>13</td>
+          <td>MQ-MC-CMD-001</td>
+          <td class="text-left">Comunicado</td>
+          <td>
+            <a href="archivos/MQ-MC-CMD-001.docx" target="_blank"> 
+              <button type="button" class="btn btn-xs btn-success">Ver</button>
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td>14  </td>
+          <td>MQ-MC-CIR-001</td>
+          <td class="text-left">Circular</td>
+          <td>
+            <a href="archivos/MQ-MC-CIR-001.docx" target="_blank"> 
+              <button type="button" class="btn btn-xs btn-success">Ver</button>
+            </a>
+          </td>
+        </tr>
+        
+        
+      </tbody>
+    </table>
+  </div>
